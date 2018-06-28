97692b63c98b1dbf677a5e32eccceaaa2f917e5b
jaybojaybojaybo <jasun.feddema@gmail.com>
2018-06-28 12:34:21 -0700

    get user authorization and user objects created. still getting error for CollectionReference.doc() function.

--- src/store.js
+++ src/store.js
@@ -1,16 +1,29 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+const fb = require('./firebaseConfig.js')
 
 Vue.use(Vuex)
 
 export const store = new Vuex.Store({
     state: {
-        
+        currentUser: null,
+        userProfile: {}
     },
     actions: {
-        
+        fetchUserProfile({ commit, state }) {
+            fb.usersCollection.doc(state.currentUser.uid).get().then(res => {
+                commit('setUserProfile', res.data())
+            }).catch(err => {
+                console.log(err)
+            })
+        }
     }, 
     mutations: {
-        
+        setCurrentUser(state, val) {
+            state.currentUser = val
+        },
+        setUserProfile(state, val) {
+            state.userProfile = val
+        }
     }
 })
